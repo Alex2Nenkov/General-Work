fc7747b0286289543a90a61c0ca57e4dea7a2aa3
Alex2Nenkov <0nenka45@solent.ac.uk>
2025-04-30 12:32:46 +0100

    start

--- Code/Class.py
+++ Code/Class.py
@@ -1,1 +1,11 @@
-print("Introduction to Python")
+import sqlite3
+db = sqlite3.connect('/Users/alexnenkov/Desktop/School/Y1/Intro to Databases/SQLite 3/MySportFinal.db')
+cursor = db.cursor()
+sql_query = "SELECT * \
+		   FROM emp"
+cursor.execute(sql_query)
+all_emp_rows = cursor.fetchall()
+for emp_row in all_emp_rows:
+    print(emp_row)
+db.close()
+
